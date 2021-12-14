d024125e8d2919fc4ae5a19b3d3a310586bc6a39
Leonardo Modro <leonardo.modro@cpb.com.br>
2021-12-14 13:19:00 -0300

    Initial Commit

---
+++ Country/CountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryApp.swift
+//  Country
+//
+//  Created by Leonardo Modro on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
